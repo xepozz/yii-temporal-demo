66890b4ca66fb9fc264d4ea966557965ea1d298a
Dmitrii Derepko <xepozz@list.ru>
2022-11-09 21:13:34 +0300

    Fix for broken translator

--- config/common/cache.php
+++ config/common/cache.php
@@ -2,7 +2,10 @@
 
 declare(strict_types=1);
 
+use Yiisoft\Validator\IdMessageReader;
+
 return [
+    \Yiisoft\Translator\MessageReaderInterface::class =>IdMessageReader::class,
     \Yiisoft\Cache\CacheInterface::class => \Yiisoft\Cache\Cache::class,
     \Psr\SimpleCache\CacheInterface::class => \Yiisoft\Cache\File\FileCache::class,
 ];
